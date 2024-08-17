fcaf8508f8a63e6d0cdafe61ebc232b4489ff959
HtonFirst <al.kozlov.rus@gmail.com>
2024-08-17 19:58:44 +0300

    init with files

---
+++ src/main/java/com/alkozlov/microSOne/MicroSOneApplication.java
@@ -0,0 +1,13 @@
+package com.alkozlov.microSOne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroSOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroSOneApplication.class, args);
+	}
+
+}
