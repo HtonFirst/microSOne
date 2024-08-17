2700c3a4f4a3fa8636ef20604284837fcd60ec2f
HtonFirst <al.kozlov.rus@gmail.com>
2024-08-17 22:35:18 +0300

    Added Cat.class and added server.port property

---
+++ src/main/java/com/alkozlov/microSOne/entity/Cat.java
@@ -0,0 +1,17 @@
+package com.alkozlov.microSOne.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Cat {
+
+    private String name;
+    private int age;
+    private int weight;
+}
